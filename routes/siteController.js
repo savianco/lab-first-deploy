c631b0dc450f5652d91ca8bb40d5cbcbebc81b20
Saturnino Villaverde <savianco@hotmail.com>
2017-02-15 13:04:13 +0100

    modify package.json

--- routes/siteController.js
+++ routes/siteController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const siteController = express.Router();
 
 siteController.get("/", (req, res, next) => {
-  res.render("site/index", { apiKey: "AIzaSyCTxsFM2Bbm4KEtuFAmdu11WF3s3zUliwU" });
+  res.render("site/index", { apiKey: "AIzaSyAq5Lf43W1-tsNgFUmWG2WFi8zcGqlxZrI" });
 });
 
 module.exports = siteController;
